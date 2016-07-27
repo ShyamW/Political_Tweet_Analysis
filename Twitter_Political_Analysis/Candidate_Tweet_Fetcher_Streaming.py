5cbfde1015224c6f095ea4d6fcb0451d3757e6ca
ShyamThiagarajan <shyam.thiagarajan@gmail.com>
2016-07-27 00:05:04 -0400

    Added Streaming component

---
+++ Twitter_Political_Analysis/Candidate_Tweet_Fetcher_Streaming.py
@@ -0,0 +1,97 @@
+import tweepy
+from tweepy import Stream
+import Log
+import ConfigParser
+from datetime import *
+from StdOutListener import StdOutListener
+
+"""
+@author
+Shyam Thiagarajan
+
+Python Program that fetches tweets from a timeline of political candidates.
+"""
+consumer_key = ''
+consumer_secret = ''
+key = ''
+secret = ''
+tweet_count = 0
+
+
+"""Reads parameters specified in config file. If no config file found, will use default values
+@updates global defaults
+"""
+def readFromConfig():
+    global consumer_key
+    global consumer_secret
+    global key
+    global secret
+    try:
+        config = ConfigParser.ConfigParser()
+        config.read('CONFIG.ini')
+        consumer_key = (config.get('TwitterInfo', 'consumer_key'))
+        consumer_secret = (config.get('TwitterInfo', 'consumer_secret'))
+        key = (config.get('TwitterInfo', 'key'))
+        secret = (config.get('TwitterInfo', 'secret'))
+        Log.record('\tSuccessfully read Config File')
+    except:
+        Log.record('INCORRECT Config, REVERTING BACK TO DEFAULTS')
+
+
+"""Logs in to Twitter using credentials.
+@return api
+    authenticated Tweepy object"""
+def loginToTwitter():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(key, secret)
+    api = tweepy.API(auth)
+    Log.record('\tSuccessfully Logged into Twitter')
+    return api
+
+
+"""Logs time and application divider in Log file"""
+def logEvents():
+    Log.record('-' * 200)
+    time = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "-04")
+    Log.record('\tApplication Ran at: ' + time)
+
+
+"""Outputs tweet data to output file
+@param candidate_tweets
+    list of candidate's tweets
+@updates output file content"""
+def outputTweets(candidate_tweets):
+    out = open('candidate_tweets.txt', 'a')
+    for tweet in candidate_tweets:
+        post = tweet.text
+        date_time = tweet.date_time
+        author = tweet.author
+        location = tweet.source_loc
+        output_data = [post, date_time, author, location]
+        out.write(str(output_data) + '\n')
+
+
+"""Gets tweets from timeline, gets metadata, and processes them.
+@requires
+    successful login to Twitter
+"""
+def streamTweets():
+    Log.record('\tWill Read From Config')
+    readFromConfig()
+    Log.record('\tWill Log into Twitter')
+    api = loginToTwitter()
+    Log.record('\tWill Stream Tweets from Twitter')
+    stream = Stream(api.auth, StdOutListener())
+    stream.userstream(_with='followings')
+
+
+"""Main Method that updates outage information to a database. Processes 15 tweets"""
+def main():
+    logEvents()
+    streamTweets()
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ Twitter_Political_Analysis/StdOutListener.py
@@ -0,0 +1,14 @@
+from tweepy.streaming import StreamListener
+from Candidate_Tweet import CandidateTweet
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        print data
+        CandidateTweet(data)
+        return True
+
+    def on_error(self, status):
+        print status
