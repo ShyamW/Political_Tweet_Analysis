cc199769fde939321a4b478d3f9c7536d1494538
root <shyam.thiagarajan@gmail.com>
2016-07-29 16:05:22 -0400

    Added Class to Analysis

--- Twitter_Political_Analysis/StdOutListener.py
+++ Twitter_Political_Analysis/StdOutListener.py
@@ -2,9 +2,7 @@ from tweepy.streaming import StreamListener
 from Candidate_Tweet import CandidateTweet
 
 class StdOutListener(StreamListener):
-    """ A listener handles tweets that are received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
-    """
+    """ A listener handles tweets that are received from the stream."""
     def on_status(self, status):
         try:
             print '!!!'
