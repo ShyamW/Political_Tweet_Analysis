8d8b65a812cf86f4b319bd38b5c28cb0487cf54b
ShyamThiagarajan <shyam.thiagarajan@gmail.com>
2016-07-30 18:35:40 -0400

    Cleaned up TweetAnalysis and added more tweets

--- Tweet_Analysis/wordz.py
+++ Tweet_Analysis/wordz.py
@@ -1,31 +1,45 @@
-# find word frequencies, word clouds, what words candidates end tweets with, remove punctuation later
+""" find word frequencies, word clouds, what words candidates end tweets with, remove punctuation later """
 
 
-class tweets:
-    def __init__(self, filename):
+class TweetAnalysis:
+    def __init__(self):
         self.tweet_list = []
         self.date_list = []
         self.person_list = []
         self.location_list = []
-        self.create_tweet_lists(filename)
+        self.file_name = ''
 
 
+    """Sets the file path to read tweet data from.
+    @param self
+        TweetAnalysis object
+    @param file_path
+        path of data file
+    @updates self.file_name"""
+    def setFilePath(self, file_path):
+        self.file_name = file_path
 
-    """Parses through Tweet_Analysis list and creates several lists corresponding to Tweet_Analysis attributes"""
-    def create_tweet_lists(self, filename):
-        with open(filename, 'r') as f:
-            for tweet in f:
-                tweet = tweet.strip('\n')
-                tweet = eval(tweet)
-                fixed_tweet = self.fixTweet(tweet[0])
+
+    """Parses through @code data_file and creates several lists corresponding to Tweet_Analysis attributes
+    @param self
+        Tweet Analysis Object"""
+    def create_tweet_lists(self):
+        with open(self.file_name, 'r') as data_file:
+            for tweet_info in data_file:
+                tweet_info = tweet_info.strip('\n')
+                tweet_info = eval(tweet_info)
+                print 'AAA'
+                print tweet_info
+                print 'AAA'
+                fixed_tweet = self.fixTweet(tweet_info[0])
                 self.tweet_list.append(fixed_tweet)
-                self.date_list.append(tweet[1])
-                self.person_list.append(tweet[2])
-                self.location_list.append(tweet[3])
+                self.date_list.append(tweet_info[1])
+                self.person_list.append(tweet_info[2])
+                self.location_list.append(tweet_info[3])
 
     """Removes punctuation, URLS etc. from Tweet_Analysis"""
     def fixTweet(self, tweet):
-        fixed_tweet = tweet.replace(',','').replace('.','').strip('\n').strip('\r').replace('  ',' ')
+        fixed_tweet = tweet.replace(',','').replace('.','').strip('\n').strip('\r').replace('  ',' ').decode('utf-8')
         print fixed_tweet
         return fixed_tweet
 
@@ -76,12 +90,13 @@ class tweets:
 
 
 def main():
-    initial_filename = '/home/dragon/Programs/TwitterApp/Tweet_Analysis/test.txt'
-    test = tweets(initial_filename)
+    test = TweetAnalysis()
+    test.setFilePath('/home/dragon/Programs/TwitterApp/Twitter_Political_Analysis/candidate_tweets.txt')
+    test.create_tweet_lists()
     print(test.getTweetFrequencies())
     candidates_word_maps = test.get_word_frequencies()
     print candidates_word_maps
-    for keys,values in candidates_word_maps.items():
+    for keys, values in candidates_word_maps.items():
         print keys
         print values
 
